ff52f153dda21b342ca65998f449ab4e12923ad1
GuidoC <148884974+Guidoc72@users.noreply.github.com>
2024-02-18 16:45:11 +0100

    Creazione Progetto finale - Corso Java PIW01

---
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/ProjectWorkPiw012024Application.java
@@ -0,0 +1,13 @@
+package it.akt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkPiw012024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkPiw012024Application.class, args);
+	}
+
+}
---
+++ ProjectWork_PIW01_2024/src/test/java/it/akt/ProjectWorkPiw012024ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.akt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkPiw012024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
