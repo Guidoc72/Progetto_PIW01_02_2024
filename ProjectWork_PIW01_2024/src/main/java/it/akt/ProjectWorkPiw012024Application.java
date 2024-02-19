6f05c60f314075f0b6ca9167cf97686344d09d68
Sadsa993 <edoardozhu@gmail.com>
2024-02-19 10:33:50 +0100

    hello

--- ProjectWork_PIW01_2024/src/main/java/it/akt/ProjectWorkPiw012024Application.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/ProjectWorkPiw012024Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectWorkPiw012024Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWorkPiw012024Application.class, args);
 	}
