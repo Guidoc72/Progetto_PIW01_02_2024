4060db3c2215cdd6afe8b3be6e8462aeedda75bf
Nicola <nsprogramwork@mail.com>
2024-02-20 15:35:49 +0100

    eliminazione import non utilizzati

--- ProjectWork_PIW01_2024/src/main/java/it/akt/models/Domanda.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/models/Domanda.java
@@ -1,12 +1,10 @@
 package it.akt.models;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Id;
