d613c931839457038825f50d39fed4cb567a714f
Samuele <samueleromanelli100@gmail.com>
2024-02-20 15:54:21 +0100

    agg table temaquiz

--- ProjectWork_PIW01_2024/src/main/java/it/akt/models/TemaQuiz.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/models/TemaQuiz.java
@@ -1,6 +1,6 @@
 package it.akt.models;
 
-import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "tema")
@@ -20,20 +21,27 @@ public class TemaQuiz {
     private Long id;
 
     //indica il nome del tema
-    @Column(name = "nome")
+    @Column(name = "nome", nullable = false)
+    @NotEmpty
     private String nome;
 
     // Relazione uno a molti con Quiz
     @OneToMany
-    private List<Quiz> quizList;
+    private Set<Quiz> quiz;
 
-    //costruttore
-	public TemaQuiz(Long id, String nome, List<Quiz> quizList) {
-		this.id = id;
-		this.nome = nome;
-		this.quizList = quizList;
-	}
+    //Relazione uno a molti con Domanda
+    @OneToMany
+    private Set<Domanda> domande;
     
+    //costruttore
+    public TemaQuiz(Long id, @NotEmpty String nome, Set<Quiz> quiz, Set<Domanda> domande) {
+    	super();
+    	this.id = id;
+    	this.nome = nome;
+    	this.quiz = quiz;
+    	this.domande = domande;
+    }
+	
 	public TemaQuiz() {}
 
 	//getter and setter
@@ -44,27 +52,35 @@ public class TemaQuiz {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	public String getNomeTema() {
+	
+	public String getNome() {
 		return nome;
 	}
 
-	public void setNomeTema(String nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public List<Quiz> getQuizList() {
-		return quizList;
+	public Set<Quiz> getQuiz() {
+		return quiz;
 	}
 
-	public void setQuizList(List<Quiz> quizList) {
-		this.quizList = quizList;
+	public void setQuiz(Set<Quiz> quiz) {
+		this.quiz = quiz;
 	}
 	
+	public Set<Domanda> getDomande() {
+		return domande;
+	}
+
+	public void setDomande(Set<Domanda> domande) {
+		this.domande = domande;
+	}
+
 	//ToString
 	@Override
 	public String toString() {
-		return "TemaQuiz [id=" + id + ", nome=" + nome + ", quizList=" + quizList + "]";
+		return "TemaQuiz [id=" + id + ", nome=" + nome + ", quizList=" + quiz + "]";
 	}
 	
 }
