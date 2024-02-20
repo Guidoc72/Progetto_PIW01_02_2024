79a7f91d2ee623a20fcb00cdd176b9cac1200df0
Samuele <samueleromanelli100@gmail.com>
2024-02-20 15:50:25 +0100

    UPDATE TABLE (TemaQuiz, Quiz) | UPDATE SCRIPT (script_sql.sql)

--- ProjectWork_PIW01_2024/src/main/java/it/akt/models/Aula.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/models/Aula.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -38,7 +39,7 @@ public class Aula {
 				inverseJoinColumns = @JoinColumn(name = "utente_id", 
 				referencedColumnName = "id"))
 	private Set<Utente> utenti = new HashSet<>();
-
+	
 	public Aula() {
 	}
 
