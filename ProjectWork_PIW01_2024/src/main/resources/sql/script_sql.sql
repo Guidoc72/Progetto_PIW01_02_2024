959ede2b30e8cb869d544505443330d8547c8c85
Samuele <samueleromanelli100@gmail.com>
2024-02-20 09:49:33 +0100

    agg file script_sql.sql

--- ProjectWork_PIW01_2024/src/main/resources/sql/script_sql.sql
+++ ProjectWork_PIW01_2024/src/main/resources/sql/script_sql.sql
@@ -1,39 +1,5 @@
-<<<<<<< HEAD
 CREATE DATABASE IF NOT EXISTS QuizDB;
 USE QuizDB;
-
-create table if not exists domanda (
-id int primary key auto_increment,
-quesito varchar (1000) NOT NULL,
-risposta1 varchar(1000) not null,
-risposta2 varchar (1000) not null,
-risposta3 varchar (1000) not null,
-risposta4 varchar (1000) not null,
-rispostaGiusta int not null,
-id_tema int not null,
-FOREIGN KEY (id_tema) REFERENCES temaquiz(id)
-);
-create table if not exists quiz_has_domanda (
-quiz_id int not null,
-domanda_id int not null,
-PRIMARY KEY (quiz_id, domanda_id),
-FOREIGN KEY (quiz_id) REFERENCES quiz(id),   
- FOREIGN KEY (domanda_id) REFERENCES domanda(id)
-);
-
--- Creazione della tabella TemaQuiz
-CREATE TABLE temaquiz (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nometema VARCHAR(255) NOT NULL
-);
-
--- Creazione della tabella Quiz con chiave esterna verso TemaQuiz
-CREATE TABLE quiz (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    data DATE,
-    tema_quiz_id INT,
-    FOREIGN KEY (tema_quiz_id) REFERENCES temaquiz(id)
-);
 
 CREATE TABLE IF NOT EXISTS aula (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -117,8 +83,6 @@ create table if not exists quiz_has_domanda (
     foreign key (domanda_id) references domanda(id)
     );
 
-=======
-
 CREATE TABLE IF NOT EXISTS utente(
     id 			int not null auto_increment,
     nome 		varchar(45) not null,
@@ -142,4 +106,3 @@ CREATE TABLE IF NOT EXISTS quiz_has_utente(
     on delete no action
     on update cascade
     );
->>>>>>> refs/heads/Emanuele-Rodrigo
