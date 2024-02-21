4f9b0c4e53010c3b1ba8104bab206eb0f920af35
Samuele <samueleromanelli100@gmail.com>
2024-02-21 10:13:41 +0100

    agg QuizRepository

--- ProjectWork_PIW01_2024/src/main/java/it/akt/repositories/QuizRepository.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/repositories/QuizRepository.java
@@ -1,12 +1,21 @@
 package it.akt.repositories;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.repository.ListCrudRepository;
+
+import it.akt.models.Aula;
 import it.akt.models.Quiz;
 
 //Creazione Repository Table Quiz
 public interface QuizRepository extends ListCrudRepository<Quiz, Long> {
-    
 	
+	//Restituisce le domande associate ad un Quiz
+	Set<Domanda> findDomandeByQuiz(Quiz quiz);
 	
+	//Restituisce le Aule asscoiate ad un Quiz 
+	Set<Aula> findAuleByQuiz(Quiz quiz);
+
 	
 }
