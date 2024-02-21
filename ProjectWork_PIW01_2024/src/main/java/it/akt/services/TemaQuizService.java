b88b5eda2e326a55fecdefbabde4722640013adc
federico <fede.g@mail.com>
2024-02-21 15:21:21 +0100

    Aggiornamento TemaQuizRepository e TemaQuizService

--- ProjectWork_PIW01_2024/src/main/java/it/akt/services/TemaQuizService.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/services/TemaQuizService.java
@@ -4,33 +4,66 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import it.akt.models.Quiz;
 import it.akt.models.TemaQuiz;
 import it.akt.repositories.TemaQuizRepository;
 
 @Service
 public class TemaQuizService {
 
-    private final TemaQuizRepository temaQuizRepository;
-
+    private TemaQuizRepository temaQuizRepository;
 
     public TemaQuizService(TemaQuizRepository temaQuizRepository) {
         this.temaQuizRepository = temaQuizRepository;
     }
 
-    // Recupero dei dati
-    public List<TemaQuiz> getAllTemiQuiz() {
-        return temaQuizRepository.findAll();
-    }
-
-    // Creazione e gestione dei temi dei quiz
-    public TemaQuiz createTemaQuiz(TemaQuiz temaQuiz) {
+/**
+ * Crea e aggiunge un tema al database e restituisce un oggetto di tipo TemaQuiz
+ * @param temaQuiz TemaQuiz object
+ * @return temaQuiz TemaQuiz object
+ */
+    
+    public TemaQuiz createTema(TemaQuiz temaQuiz) {
         return temaQuizRepository.save(temaQuiz);
     }
 
-    public TemaQuiz updateTemaQuiz(TemaQuiz temaQuiz) {
+    /**
+     * Restituisce una lista di tutti i temi
+     * @return List temaQuiz object
+     */
+    public List<TemaQuiz> getAllTemi() {
+    	return temaQuizRepository.findAll();
+    }
+    
+/**
+ * Modifica il tema nel database e restituisce l'oggetto
+ * @param temaQuiz TemaQuiz object
+ * @return temaQuiz TemaQuiz object
+ */
+    public TemaQuiz updateTema(TemaQuiz temaQuiz) {
         return temaQuizRepository.save(temaQuiz);
     }
 
+/**
+ * Trova un tema tramite il suo id
+ * @param id temaQuiz object
+ * @return temaQuiz TemaQuiz object
+ */
+    public TemaQuiz getTemaById(Long id) {
+        return temaQuizRepository.findById(id).orElseThrow(() -> 
+        		new RuntimeException(String.format("Non esiste nessun tema con id: %d", id)
+        		));
+    } 
+/**
+ * Elimina un tema tramite il suo id
+ * @param id temaQuiz object
+ */
+
+    public void deleteTemaById(Long id) {
+        temaQuizRepository.deleteById(id);
+    }
+    
+    
     // 3. Associazione tra temi e quiz
 //    @Transactional
 //    public void associateQuizWithTema(Long temaId, List<Quiz> quizList) {
