b53f3869cd0728521f093abc6e2bd1cc9384291d
Sadsa993 <edoardozhu@gmail.com>
2024-02-22 09:25:23 +0100

    repo and serv

---
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/services/RisultatoService.java
@@ -0,0 +1,40 @@
+package it.akt.services;
+
+
+import it.akt.models.Risultato;
+import it.akt.repositories.RisultatoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RisultatoService {
+    private RisultatoRepository risultatoRepository;
+
+
+
+    public void addRisultato(Risultato risultato) {
+        risultatoRepository.save(risultato);
+    }
+
+    public void deleteRisultatoById(Long id) {
+        risultatoRepository.deleteById(id);
+    }
+
+    public List<Risultato> findAllByQuiz(Long id) {
+        return risultatoRepository.findByQuizId(id).orElseThrow(() ->
+                new RuntimeException(String.format("Non ci sta nessuna risposta per il Quiz con id %d", id) ));
+
+    }
+    public List<Risultato> findAllByUtenteId(Long id) {
+        return risultatoRepository.findAllByUtenteId(id).orElseThrow(() ->
+                new RuntimeException(String.format("Questo utente con id %d non ha nessuna risposta", id) ));
+
+    }
+
+    public Risultato findByUtenteIdAndQuizId(Long utente_id, Long quiz_id) {
+        return risultatoRepository.findByUtenteIdAndQuizId(utente_id, quiz_id).orElseThrow(() ->
+                new RuntimeException(String.format("Questo utente con id %d non ha una risposta al quiz con id : %d", utente_id, quiz_id) ));
+    }
+
+}
