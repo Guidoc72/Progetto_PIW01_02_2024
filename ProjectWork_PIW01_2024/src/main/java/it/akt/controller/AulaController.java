8661db7e839ad0d30045faa4332300e54373127b
riccardo <riccardo.lapietra@yahoo.com>
2024-02-26 14:25:00 +0100

    Merge branch 'Rosa-Riccardo' into testRepositoriesService

--- ProjectWork_PIW01_2024/src/main/java/it/akt/controller/AulaController.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/controller/AulaController.java
@@ -1,5 +1,19 @@
 package it.akt.controller;
 
-public class AulaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import it.akt.models.Aula;
+
+@Controller
+public class AulaController {
+@GetMapping("/")
+public String getAula (Model model) {
+	Aula aula = new Aula();
+	model.addAttribute("nomeAula", aula);
+	return "aula";
+	
+	
+}
 }
