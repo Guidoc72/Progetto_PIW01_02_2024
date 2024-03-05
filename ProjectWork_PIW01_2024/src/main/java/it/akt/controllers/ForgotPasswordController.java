570be393c75a3f1e07a5ca8fa29b07d7d696cde6
valerio <valerioruggieri@hotmail.com>
2024-03-05 10:32:46 +0100

    update controller

--- ProjectWork_PIW01_2024/src/main/java/it/akt/controllers/ForgotPasswordController.java
+++ ProjectWork_PIW01_2024/src/main/java/it/akt/controllers/ForgotPasswordController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,7 +29,7 @@ import net.bytebuddy.utility.RandomString;
  * 
  * @author Rodrigo
  */
-
+@Controller
 public class ForgotPasswordController {
 
 
